50a364618a9638d64c3e3e83f2aff70cdbec3cf1
meng <371562656@qq.com>
2026-04-16 09:10:27 +0800

    线上访问域名一致

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,8 +19,7 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://mengdal.github.io',
+  url: 'https://docs.lmgateway.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
